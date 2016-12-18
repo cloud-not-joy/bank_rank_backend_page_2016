0b0615be7d950bbe7b866d131a75d582c868b8d4
liangjing <liangjing@codoon.com>
2016-12-18 18:57:07 +0800

    staff view ui ok

--- app/scripts/staff.js
+++ app/scripts/staff.js
@@ -2,12 +2,33 @@ var staffView = Vue.extend({
   template: $("#staff-template").html(),
   data: function() {
     return {
-      username: '',
-      password: ''
+      currentGoods: {
+
+      },
+      goodsArray: []
     }
   },
+  mounted: function() {
+    this.goodsArray = [{
+      imgUrl: 'http://imgsize.ph.126.net/?imgurl=http://img1.ph.126.net/pKa8Kttj0AsOd7TijP9CRA==/6632036435442880504.jpg_188x188x1.jpg',
+      title: '吹风机一台',
+      rank: 400
+    }];
+  },
   methods: {
-
+    exchange: function() {
+      // TODO 获取当前选中的商品信息
+      $(".exchange-confirm").modal('show');
+    },
+    sureConfirm: function() {
+      $(".exchange-ok").modal('show');
+    },
+    closeConfirm: function() {
+      $(".exchange-modal").modal('hide');
+    },
+    showExchangeHistory: function() {
+      $(".exchange-history").modal('show');
+    }
   }
 })
 
